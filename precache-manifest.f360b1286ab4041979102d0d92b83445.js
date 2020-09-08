7a01f17e74a1042b244d150542e0e24b9b3d14cb
Milda Norkute <milda.norkute@thomsonreuters.com>
2020-09-08 16:33:30 +0200

    Updates

---
+++ precache-manifest.f360b1286ab4041979102d0d92b83445.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1ca8145436dd9d5f1503ddd80ae8a2",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "625faf322fe9506dd7aa",
+    "url": "/portfolio2020/static/css/main.2c4e4a20.chunk.css"
+  },
+  {
+    "revision": "765bfe914abd50d4694a",
+    "url": "/portfolio2020/static/js/2.3911c056.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/portfolio2020/static/js/2.3911c056.chunk.js.LICENSE"
+  },
+  {
+    "revision": "625faf322fe9506dd7aa",
+    "url": "/portfolio2020/static/js/main.bbf0b457.chunk.js"
+  },
+  {
+    "revision": "e1544a2aa214378f74fe",
+    "url": "/portfolio2020/static/js/runtime-main.68eda7b8.js"
+  },
+  {
+    "revision": "9b2cfa52527192d2d3a873bf37c4234a",
+    "url": "/portfolio2020/static/media/Subtle-Prism_11.9b2cfa52.svg"
+  },
+  {
+    "revision": "d9d99f3873a82c01eca82c7d859eb370",
+    "url": "/portfolio2020/static/media/TK2HOW.d9d99f38.png"
+  },
+  {
+    "revision": "6c683f32ae24981a6113932fe83df33a",
+    "url": "/portfolio2020/static/media/beer_android.6c683f32.png"
+  },
+  {
+    "revision": "8e475b448cf8a8a38115664f64fcd21a",
+    "url": "/portfolio2020/static/media/beer_clickable.8e475b44.png"
+  },
+  {
+    "revision": "c9193223dfd2ea1feaffa29867d59d42",
+    "url": "/portfolio2020/static/media/beer_ionic.c9193223.png"
+  },
+  {
+    "revision": "ffa7f7055f3bc84fcf5148fb454d3a14",
+    "url": "/portfolio2020/static/media/beer_process.ffa7f705.png"
+  },
+  {
+    "revision": "8dcc55c8364f78af2b2b3b1776e78fe5",
+    "url": "/portfolio2020/static/media/cardsort.8dcc55c8.png"
+  },
+  {
+    "revision": "af47b5ea75fedc856f8ddf9ff64121d3",
+    "url": "/portfolio2020/static/media/cern_UI.af47b5ea.png"
+  },
+  {
+    "revision": "6379d4f17eebc6291eb1944677179068",
+    "url": "/portfolio2020/static/media/cern_old.6379d4f1.png"
+  },
+  {
+    "revision": "0aa7b4d6e29eb2c5bbf3f1003c481806",
+    "url": "/portfolio2020/static/media/cern_tech.0aa7b4d6.png"
+  },
+  {
+    "revision": "2b8f33817ace91a4c55e290147b877ba",
+    "url": "/portfolio2020/static/media/cern_thumb2.2b8f3381.png"
+  },
+  {
+    "revision": "e45c7e0f8b0177b1dabc28e3789dfceb",
+    "url": "/portfolio2020/static/media/cern_web.e45c7e0f.png"
+  },
+  {
+    "revision": "f96e998ed5cd065f57d1367a53aef566",
+    "url": "/portfolio2020/static/media/giphy.f96e998e.gif"
+  },
+  {
+    "revision": "ce997f1503d2fc2307881b3d3ec4c975",
+    "url": "/portfolio2020/static/media/justabeer-story.ce997f15.png"
+  },
+  {
+    "revision": "ec281579bd1c1b78d09b985d3158b87a",
+    "url": "/portfolio2020/static/media/me_talking.ec281579.png"
+  },
+  {
+    "revision": "db36de49d239710e09a8cdb669570c3a",
+    "url": "/portfolio2020/static/media/new_site_map.db36de49.png"
+  },
+  {
+    "revision": "6d6d351c106c4dbc90ec2c943b7a5b9e",
+    "url": "/portfolio2020/static/media/tk_concept.6d6d351c.png"
+  },
+  {
+    "revision": "c70574aff46606724c97c07dc4d34ef2",
+    "url": "/portfolio2020/static/media/tk_thum3d.c70574af.png"
+  },
+  {
+    "revision": "4c5385b523576375832e5a9fa7b27854",
+    "url": "/portfolio2020/static/media/wireframes.4c5385b5.png"
+  }
+]);
